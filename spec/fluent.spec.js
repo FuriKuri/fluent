1cf3b0022eb60d0ff0b39aa45ecab272872bb9de
Theo Pack <tf.pack@gmail.com>
2015-01-03 11:27:01 +0100

    Create simple function chain

--- spec/fluent.spec.js
+++ spec/fluent.spec.js
@@ -2,15 +2,32 @@
 
 describe('fluent', function() {
 
-  var fluent;
-  beforeEach(function() {
-    fluent = require('../lib/fluent');
-  });
+  var Fluent = require('../lib/fluent');
 
-  describe('Your first test', function() {
+  describe('The method chain', function() {
 
-    it('does something meaningful', function() {
-      expect(fluent.prop1).toEqual('prop1');
+    it('can create a simple function chain with a config object', function() {
+      var config = {
+        init: ['firstFn'],
+        chain: {
+          firstFn: ['secondFn'],
+          secondFn: ['lastFn'],
+          lastFn: []
+        }
+      };
+      var fluent = new Fluent(config);
+      expect(fluent.firstFn).toBeDefined();
+      expect(fluent.firstFn().secondFn).toBeDefined();
+      expect(fluent.firstFn().secondFn().lastFn).toBeDefined();
+
+      expect(fluent.secondFn).not.toBeDefined();
+      expect(fluent.lastFn).not.toBeDefined();
+
+      expect(fluent.firstFn().firstFn).not.toBeDefined();
+      expect(fluent.firstFn().lastFn).not.toBeDefined();
+
+      expect(fluent.firstFn().secondFn().firstFn).not.toBeDefined();
+      expect(fluent.firstFn().secondFn().secondFn).not.toBeDefined();
     });
 
   });
